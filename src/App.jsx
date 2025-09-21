5ac8c1caf03cea3a9ad9c9acd97a56aeef4b0d36
Tuck <Tunchikrazy@icloud.com>
2025-09-21 14:45:53 -0500

    first try

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 // src/App.jsx
+import { useState } from "react";
+import { Routes, Route } from "react-router";
+import NavBar from "./components/NavBar/NavBar.jsx";
+//import MailboxForm from "./components/NavBar/MailboxForm/MailboxForm";
+import MailboxList from "./components/NavBar/MailboxList/MailboxList";
+//import MailboxDetails from "./components/MailboxDetails/MailboxDetails";
+
+
+
+
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+const [mailboxes, setMailboxes] = useState([]);
+
+  const addBox=(newMailboxData) => {
+    newMailboxdata._id = Mailbox.length + 1
+    setMailboxes ([...mailboxes, newMailboxData])
+  }
+  
+  return (
+    <>
+    <NavBar/>
+  <h1>React Mailboxes</h1>
+  <Routes>
+        <Route path="/" element={<main><h1>Post Office</h1></main>} />
+        <Route path="/mailboxes" element={<MailboxList mailboxes={mailboxes} />} />
+        <Route path="/new-mailbox" element={<MailboxForm addBox={addBox} />} />
+        <Route path="/mailboxes/:mailboxId" element={<MailboxDetails mailboxes={mailboxes} />} />
+  </Routes>
+  </>
+)
 };
 
 export default App;
